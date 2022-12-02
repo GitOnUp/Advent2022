743997a0efe7d6733864cecc53457615dfc7b1ee
George Murray <murray.george.m@gmail.com>
2022-12-02 09:11:39 -0800

    Day 2 part 1

--- advent/__init__.py
+++ advent/__init__.py
@@ -40,3 +40,46 @@ class Day1:
     @staticmethod
     def p2():
         print(sum(Day1.top(3)))
+
+
+class Day2:
+    @staticmethod
+    def generate_result_map():
+        result_scores = {
+            "A": {
+                "X": 3,
+                "Y": 6,
+                "Z": 0,
+            },
+            "B": {
+                "X": 0,
+                "Y": 3,
+                "Z": 6,
+            },
+            "C": {
+                "X": 6,
+                "Y": 0,
+                "Z": 3,
+            },
+        }
+        choice_scores = {
+            "X": 1,
+            "Y": 2,
+            "Z": 3
+        }
+        score_map = {}
+        for opponent_play in result_scores:
+            for my_play, my_result_score in result_scores[opponent_play].items():
+                key = f"{opponent_play} {my_play}"
+                score = my_result_score + choice_scores[my_play]
+                score_map[key] = score
+
+        return score_map
+
+    @staticmethod
+    def p1():
+        score = 0
+        result_map = Day2.generate_result_map()
+        for line in yield_lines("d2.txt"):
+            score += result_map[line]
+        print(score)
---
+++ advent/d2_1.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    from advent import Day2
+    Day2.p1()
